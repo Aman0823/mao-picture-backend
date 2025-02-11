d60c671c98b1c3928c596ffb1d38a5990ba65949
Aman0823 <3515746178@qq.com>
2025-02-11 16:32:04 +0800

    项目初始化

---
+++ src/main/java/com/example/maopicturebackend/MaoPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.maopicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaoPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaoPictureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/maopicturebackend/MaoPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maopicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaoPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
